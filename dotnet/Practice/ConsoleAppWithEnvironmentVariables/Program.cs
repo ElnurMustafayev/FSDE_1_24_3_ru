73a15a2ac882c39ea1a7983fedb49c80d6cdc513
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2025-11-25 21:03:23 +0400

    Add vscode debug configuration with .env

---
+++ dotnet/Practice/ConsoleAppWithEnvironmentVariables/Program.cs
@@ -0,0 +1,15 @@
+﻿var envVariables = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Process);
+
+foreach (var item in envVariables)
+{
+    System.Console.WriteLine(item);
+}
+
+
+// while (true)
+// {
+//     var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+//     ArgumentNullException.ThrowIfNullOrWhiteSpace(connectionString);
+//     System.Console.WriteLine($"connection string: {connectionString}");
+//     Thread.Sleep(1000);
+// }
